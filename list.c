dbdb18e093562ec9074f22c4ba765c822133605e
Replit user <>
2023-08-25 00:29:56 +0000

    new try

--- list.c
+++ list.c
@@ -41,7 +41,7 @@ void * firstList(List * list) {
   if (list->head==NULL) return NULL;
   *list->current=*list->head;
   
-  return list->current;
+  return list->current+1;
 }
 
 void * nextList(List * list) {
